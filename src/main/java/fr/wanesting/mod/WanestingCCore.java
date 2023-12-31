51b89acffc89bf6e61742a2b326c8d87466516e5
Seynax <seyronax@gmail.com>
2023-12-31 19:18:31 +0100

    Initialize setup

---
+++ src/main/java/fr/wanesting/mod/WanestingCCore.java
@@ -0,0 +1,31 @@
+package fr.wanesting.mod;
+
+import com.mojang.logging.LogUtils;
+import net.minecraft.client.Minecraft;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.RenderItemInFrameEvent;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.server.ServerStartingEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.ModLoadingContext;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+@Mod(WanestingMod.MODID)
+public class WanestingMod {
+    public static final String MODID = "mod";
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public WanestingCCore() {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        MinecraftForge.EVENT_BUS.register(this);
+
+        ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, Config.SPEC);
+    }
+}
